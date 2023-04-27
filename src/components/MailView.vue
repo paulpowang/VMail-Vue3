279693539277e9e3f95bd3820dcd39df7f8b14eb
Paul Wang <wang.po.hung@gmail.com>
2023-04-26 17:21:17 -0700

    add MailView.vue

---
+++ src/components/MailView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="email-display">
+    <h2>
+      Subject: <strong>{{ email.subject }}</strong>
+    </h2>
+    <div>
+      <em>From {{ email.from }} on {{ format(new Date(email.sentAt), "MMM do yyyy") }}</em>
+    </div>
+    <div v-html="marked(email.body)" />
+  </div>
+</template>
+
+<script>
+import { format } from "date-fns";
+import marked from "marked";
+export default {
+  name: "MailView",
+  props: {
+    email: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return {
+      format,
+      marked,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
